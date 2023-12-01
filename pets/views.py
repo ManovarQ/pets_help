f2d17163c8f23858f83aa45e583852a6618be71a
ManovarQ <napolches@gmail.com>
2023-12-01 14:05:44 +0300

    posts_for_all_without_comments

--- pets/views.py
+++ pets/views.py
@@ -2,12 +2,11 @@ from django.shortcuts import render, redirect
 from django.views.generic.base import View
 from .models import Post
 from .form import CommentsForm
-# from .forms import PostForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic, View
 
-# Create your views here.
+
 class PostView(View):
     def get(self, request):
         posts = Post.objects.all()
@@ -29,26 +28,6 @@ class AddComments(View):
             form.save()
         return redirect(f'/{pk}')
 
-
-# def create_post(request):
-#     if request.method == "POST":
-#         formset = PostForm(request.POST, request.FILES)
-#         if formset.is_valid():
-#             formset.save()
-#             return HttpResponseRedirect(('/'))
-#         else:
-#             raise ValueError('Some logic broken')
-#
-#     elif request.method == "GET":
-#         post_form = PostForm()
-#         return render(request, "pets/create_post.html", context={"form":post_form})
-#     else:
-#         raise ValueError(f'Method {request.method} is not supported')
-
-# class PostFormView(generic.FormView):
-#     template_name = 'pets/create_post.html'
-#     form_class = PostForm
-#     success_url = '/'
 
 class PostCreateView(generic.CreateView):
     model = Post
